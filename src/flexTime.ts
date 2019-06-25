215992bc742090b9a85da2a23dd21cbc749d539d
Erik Fortune <pc@erik.org>
2019-06-24 20:52:27 -0700

    Initial checkin. Ported FlexTime to typescript

---
+++ src/flexTime.ts
@@ -0,0 +1,239 @@
+"use strict";
+
+import TimeOfDay from "typings/TimeOfDay/TimeOfDay";
+import TimeOfDayProvider from "typings/TimeOfDayProvider/TimeOfDayProvider";
+
+// flexTimeRegex puts each component into a capture group and matches the entire line ignoring leading and trailing whitespace
+// substringRegex matches anywhere into a single capture group
+// flexTimeRegex is for internal use - substringRegex is for use by consumers who want to embed a flex time into another regex
+
+const flexTimeRegex = /^\s*(\d?\d):?(\d\d)\s*(a|A|am|AM|p|P|pm|PM)?\s*$/;
+const flexTimeSubstringRegex = /((?:\d?\d):?(?:\d\d)\s*(?:a|A|am|AM|p|P|pm|PM)?)/;
+
+function findFutureHour(now, hours, minutes): number {
+    hours = (hours === 12) ? 0 : hours;
+
+    const nowHours = now.getHours();
+    while (hours < nowHours) {
+        hours += 12;
+    }
+
+    if ((hours === nowHours) && (minutes < now.getMinutes())) {
+        hours += 12;
+    }
+
+    return (hours % 24);
+}
+
+function _tryGetValuesFromString(str: string, now?: TimeOfDayProvider): TimeOfDay {
+    const match = flexTimeRegex.exec(str);
+
+    if (match !== null) {
+        let hours = Number(match[1]);
+        let minutes = Number(match[2]);
+
+        if ((hours >= 0) && (hours < 24) && (minutes >= 0) && (minutes < 60)) {
+            let isValid = true;
+
+            // 12 hour time is unambiguous if am/pm is specified
+            const hasAmPm = (typeof match[3] === "string") && ((match[3].length === 1) || (match[3].length === 2));
+
+            // 24 hour time is unambiguous if there is no am/pm and the hour is 0, greater than 12 or has an extra
+            // leading 0.   So "0800" is unambiguously 8 in the morning but "800" could be either morning or evening
+            const isUnambiguous24hrTime = (!hasAmPm) && ((hours === 0) || (hours > 12) || match[1][0] === "0");
+
+            if (hasAmPm) {
+                // Make sure we aren't trying to use am/pm for unambiguous 24 hour times
+                isValid = isValid && ((hours > 0) && (hours <= 12));
+                if (isValid) {
+                    // get to 24 hour time. 12 am === 00:00
+                    hours = (hours === 12) ? 0 : hours;
+                    if (match[3].toLowerCase()[0] === "p") {
+                        hours = hours + 12;
+                    }
+                }
+            }
+
+            // Here comes the "flex" part. If the user entered a possible 12-hour time
+            // without am/pm then we assume they mean the upcoming one (e.g. "830" means
+            // "0830" if "now" is 7 in the morning but it means "2030" if now is noon)
+            // Gets a little weird across midnight (e.g. 130 means 0130 if now is 2300)
+            if (isValid && (!hasAmPm) && (!isUnambiguous24hrTime)) {
+                hours = findFutureHour(now, hours, minutes);
+            }
+
+            if (isValid) {
+                return { hours: hours, minutes: minutes };
+            }
+        }
+    }
+    return undefined;
+}
+
+/**
+ * Converts a flexible time specification into the nearest future time.
+ */
+export default class FlexTime implements TimeOfDayProvider, TimeOfDay {
+    private _hours: number;
+    private _minutes: number;
+
+    /**
+     * Converts a flexible time specification into a specific hour and minute
+     * Examples:
+     * 24-hour time: 0100, 1430
+     * 12-hour time: 12:10p, 9:00a
+     * FlexTime or Date object
+     * Number (time in ticks)
+     * Ambiguous times (e.g. "9:00") are converted to the time after the
+     * starting date (so at 8am "9:00" refers to 9am, but at 10am "9:00" is 9pm)
+     * @param {string|number|TimeOfDayProvider} [init] - Initializer to use for the date
+     * @param {Date} [now] -  Optional start time. Defaults to Date.now()
+     */
+    public constructor(init?: string|number|TimeOfDayProvider, now?: TimeOfDayProvider) {
+        now = now || new Date();
+
+        if ((init !== undefined) && (init !== "")) {
+            if ((typeof init === "string") && (init !== "")) {
+                const result = _tryGetValuesFromString(init, now);
+                if (!result) {
+                    throw new Error(`Invalid time string "${init}".`);
+                }
+                this._hours = result.hours;
+                this._minutes = result.minutes;
+                return;
+            }
+            else if ((init instanceof FlexTime) || (init instanceof Date)) {
+                now = init;
+            }
+            else if (typeof init === "number") {
+                now = new Date(init);
+            }
+            else {
+                throw new Error(`Illegal flextime initializer ${init}`);
+            }
+        }
+
+        this._hours = now.getHours();
+        this._minutes = now.getMinutes();
+    }
+
+    /**
+     * The hours portion of the FlexTime as a 24-hour time (0..23).
+     */
+    public get hours(): number {
+        return this._hours;
+    }
+
+    /**
+     * The minutes portion of the FlexTime (0..59)
+     */
+    public get minutes(): number {
+        return this._minutes;
+    }
+
+    /**
+     * Gets the hours portion of the FlexTime as a 24-hour time (0..23)
+     */
+    public getHours(): number {
+        return this._hours;
+    }
+
+    /**
+     * Gets the minutes portion of the FlexTime (0..59)
+     */
+    public getMinutes(): number {
+        return this._minutes;
+    }
+
+    /**
+     * Gets the delta in minutes between a supplied time and this FlexTime
+     * assuming both times are on the same day.
+     * @example If this time is 23:50 and other is 00:10 the delta is -23 hours 40 minutes
+     * @param {TimeOfDayProvider} other - The time to be compared
+     */
+    public getAbsoluteDeltaInMinutes(other: TimeOfDayProvider): number {
+        const myAbsMinutes = (this._hours * 60) + this._minutes;
+        const otherAbsMinutes = (other.getHours() * 60) + (other.getMinutes());
+        return otherAbsMinutes - myAbsMinutes;
+    }
+
+    /**
+     * Gets the delta in minutes between the nearest supplied time and this FlexTime,
+     * crossing days if necessary.
+     * @example If this time is 23:50 and the other is 00:10 the delta is 20 minutes
+     * @param {TimeOfDayProvider} other - The time to be compared
+     */
+    public getDeltaInMinutes(other: TimeOfDayProvider): number {
+        const myAbsMinutes = (this._hours * 60) + this._minutes;
+        const otherAbsMinutes = (other.getHours() * 60) + (other.getMinutes());
+        let delta = otherAbsMinutes - myAbsMinutes;
+        // difference is > 12 hours so we should go the other way
+        // e.g. 0015 is 30 minutes after 2345, not 23:30 before
+        if (delta > (12 * 60)) {
+            delta = -((24 * 60) - delta);
+        }
+        else if (delta < (-12 * 60)) {
+            delta = (24 * 60) + delta;
+        }
+        return delta;
+    }
+
+    /**
+     * Converts a FlexTime to a future Date relative to now or to a specified time.
+     * @param {number} [fudgeFactorInMinutes] - Optional fudge factor in minutes applied before deciding if a date is in the past.
+     * @param {number|Date|FlexTime} [baseDate] - Optional base date, defaults to Date.now()
+     */
+    public toDate(fudgeFactorInMinutes?: number, baseDate?: number|Date): Date {
+        let date = ((typeof baseDate === "number" ? new Date(baseDate) : baseDate)) || new Date();
+        fudgeFactorInMinutes = fudgeFactorInMinutes || 0;
+
+        const delta = this.getAbsoluteDeltaInMinutes(date);
+        if ((delta - fudgeFactorInMinutes) > 0) {
+            // date is behind us, move to tomorrow
+            const dayInMilliseconds = 24 * 60 * 60 * 1000;
+            date = new Date(date.getTime() + dayInMilliseconds);
+        }
+
+        date.setHours(this._hours);
+        date.setMinutes(this._minutes);
+        return date;
+    }
+
+    /**
+     * Converts a FlexTime to a string using simple pattern replacement.
+     * @param {string} [fmt] - Optional format to use, defaults to "hh:mm tt"
+     */
+    public toString(fmt?: string): string {
+        let result = fmt || "hh:mm tt";
+        result = result.replace("HH", (this._hours < 10) ? `0${this._hours}` : `${this._hours}`);
+        result = result.replace("hh", `${((this._hours % 12) || 12)}`);
+        result = result.replace(/mm/i, `${(this._minutes < 10 ? `0${this._minutes}` : this._minutes)}`);
+        result = result.replace("TT", (this._hours < 12) ? "AM" : "PM");
+        result = result.replace("tt", (this._hours < 12) ? "am" : "pm");
+        result = result.replace("T", (this._hours < 12) ? "A" : "P");
+        result = result.replace("t", (this._hours < 12) ? "a" : "p");
+        return result;
+    }
+
+    /**
+     * Creates a FlexTime given a base time and a delta in minutes
+     * @param {number|TimeOfDayProvider} fromTime - Base time
+     * @param {number|string} [deltaInMinutes] - Delta to be applied
+     */
+    public static getFlexTime(fromTime?: number|TimeOfDayProvider, deltaInMinutes?: number|string): FlexTime {
+        let date = ((typeof fromTime === "number" ? new Date(fromTime) : fromTime)) || new Date();
+        if (date instanceof FlexTime) {
+            date = date.toDate();
+        }
+
+        if (deltaInMinutes) {
+            const minutes = Number(deltaInMinutes);
+            const deltaInMilliseconds = minutes * 60 * 1000;
+            const timeInMillis = (date as Date).getTime() + deltaInMilliseconds;
+            date = new Date(timeInMillis);
+        }
+        return new FlexTime(undefined, date);
+    }
+
+    public static substringRegex = flexTimeSubstringRegex;
+}
---
+++ test/unit/flexTime.spec.ts
@@ -0,0 +1,313 @@
+"use strict";
+
+import FlexTime from "../../src/flexTime";
+
+function makeAmbiguousTimeString(date: Date): string {
+    let hours = (date.getHours() % 12);
+    hours = ((hours === 0) ? 12 : hours);
+    let minutes = date.toTimeString().slice(3, 5);
+    return hours + minutes;
+}
+
+interface UnambiguousParseTest {
+    string: string;
+    hour: number;
+    minutes: number;
+};
+
+describe("flexTime", function (): void {
+    describe("constructor", (): void => {
+        let morning = new Date(2018, 1, 1, 6, 10);
+        let evening = new Date(2018, 1, 1, 18, 10);
+
+        it("should parse unambiguous 12- or 24-hour times without regard for time of day", (): void => {
+            [
+                { string: "0000", hour: 0, minutes: 0 },
+                { string: "0100", hour: 1, minutes: 0 },
+                { string: "0600", hour: 6, minutes: 0 },
+                { string: "0615", hour: 6, minutes: 15 },
+                { string: "1312", hour: 13, minutes: 12 },
+                { string: "13:12", hour: 13, minutes: 12 },
+                { string: "23:59", hour: 23, minutes: 59 },
+                { string: "800 am", hour: 8, minutes: 0 },
+                { string: "8:00 pm", hour: 20, minutes: 0 },
+                { string: "101 am", hour: 1, minutes: 1 },
+                { string: "11:11p", hour: 23, minutes: 11 },
+                { string: "1212a", hour: 0, minutes: 12 },
+                { string: "12:12 PM", hour: 12, minutes: 12 },
+            ].forEach(function (test: UnambiguousParseTest): void {
+                let time = new FlexTime(test.string, morning);
+                expect(time.getHours()).toBe(test.hour);
+                expect(time.getMinutes()).toBe(test.minutes);
+                expect(time.hours).toBe(test.hour);
+                expect(time.minutes).toBe(test.minutes);
+
+                time = new FlexTime(test.string, evening);
+                expect(time.getHours()).toBe(test.hour);
+                expect(time.getMinutes()).toBe(test.minutes);
+                expect(time.hours).toBe(test.hour);
+                expect(time.minutes).toBe(test.minutes);
+            });
+        });
+
+        it("should adjust ambiguous times depending on time of day", (): void => {
+            interface AmbiguousParseTest {
+                string: string;
+                morning: number;
+                evening: number;
+                minutes: number;
+            };
+
+            [
+                { string: "100", morning: 13, evening: 1, minutes: 0 },
+                { string: "605", morning: 18, evening: 6, minutes: 5 },
+                { string: "615", morning: 6, evening: 18, minutes: 15 },
+                { string: "112", morning: 13, evening: 1, minutes: 12 },
+                { string: "11:59", morning: 11, evening: 23, minutes: 59 },
+                { string: "1215", morning: 12, evening: 0, minutes: 15 },
+            ].forEach(function (test: AmbiguousParseTest): void {
+                let time = new FlexTime(test.string, morning);
+                expect(time.getHours()).toBe(test.morning);
+                expect(time.getMinutes()).toBe(test.minutes);
+
+                time = new FlexTime(test.string, evening);
+                expect(time.getHours()).toBe(test.evening);
+                expect(time.getMinutes()).toBe(test.minutes);
+            });
+        });
+
+        it("should use the current time if no time is specified", (): void => {
+            let thirtyMinutesInMilliseconds = 30 * 60 * 1000;
+            let future = new Date(Date.now() + thirtyMinutesInMilliseconds);
+            let past = new Date(Date.now() - thirtyMinutesInMilliseconds);
+
+            let time = new FlexTime(makeAmbiguousTimeString(future));
+            let expected = future.getHours();
+            expect(time.getHours()).toBe(expected);
+
+            time = new FlexTime(makeAmbiguousTimeString(past));
+            expected = (past.getHours() + 12) % 24;
+            expect(time.getHours()).toBe(expected);
+
+            let date = new Date();
+            time = new FlexTime();
+            expect(time.getHours()).toBe(date.getHours());
+            expect(time.getMinutes()).toBe(date.getMinutes());
+        });
+
+        it("should initialize from another flex time", (): void => {
+            let time = new FlexTime(new Date(2018, 1, 1, 12, 0));
+            let newTime = new FlexTime(time);
+            expect(time.getHours()).toEqual(newTime.getHours());
+            expect(time.getMinutes()).toEqual(newTime.getMinutes());
+        });
+
+        it("should initiialize from a Date object", (): void => {
+            let date = new Date();
+            let time = new FlexTime(date);
+            expect(time.getHours()).toBe(date.getHours());
+            expect(time.getMinutes()).toBe(date.getMinutes());
+        });
+
+        it("should initialize from an time in milliseconds", (): void => {
+            let date = new Date();
+            let time = new FlexTime(date.getTime());
+            expect(time.getHours()).toBe(date.getHours());
+            expect(time.getMinutes()).toBe(date.getMinutes());
+        });
+
+        it("should throw for invalid time strings", (): void => {
+            [
+                "12345", "12", "fred", "2515", "875", "123:4", "8:00 ama", "2300 am",
+            ].forEach(function (test): void {
+                expect((): FlexTime => new FlexTime(test)).toThrowError(`Invalid time string "${test}".`);
+            });
+        });
+
+        /*
+         * Disabled because typescript blocks the call
+        it("should throw for an invalid initializer", (): void => {
+            expect((): FlexTime => new FlexTime({})).toThrow();
+        });
+        */
+    });
+
+    describe("getFlexTime static method", (): void => {
+        it("should use Date, time in milliseconds, or FlexTime and delta if supplied", (): void => {
+            let now = new Date(2018, 1, 1, 11, 11, 11);
+            let nowTime = now.getTime();
+            [
+                { string: "20", hour: 11, minutes: 31 },
+                { string: "-20", hour: 10, minutes: 51 },
+                { string: "60", hour: 12, minutes: 11 },
+                { string: "120", hour: 13, minutes: 11 },
+                { string: undefined, hour: 11, minutes: 11 },
+            ].forEach(function (test: UnambiguousParseTest): void {
+                let time = FlexTime.getFlexTime(now, test.string);
+                expect(time.getHours()).toBe(test.hour);
+                expect(time.getMinutes()).toBe(test.minutes);
+
+                time = FlexTime.getFlexTime(nowTime, test.string);
+                expect(time.getHours()).toBe(test.hour);
+                expect(time.getMinutes()).toBe(test.minutes);
+
+                time = FlexTime.getFlexTime(now);
+                time = FlexTime.getFlexTime(time, test.string);
+                expect(time.getHours()).toBe(test.hour);
+                expect(time.getMinutes()).toBe(test.minutes);
+            });
+        });
+
+        it("should use the current time if no time is supplied", function (): void {
+            let flexNow = FlexTime.getFlexTime();
+            let now = new Date();
+            let dateFromFlex = new Date();
+            dateFromFlex.setHours(flexNow.getHours());
+            dateFromFlex.setMinutes(flexNow.getMinutes());
+            // 5 seconds should be plenty of fudge.  This test might also fail
+            // right at midnight if flexNow and now happen to land on different
+            // days.
+            let delta = dateFromFlex.getTime() - now.getTime();
+            expect(delta).toBeLessThan(5000);
+        });
+    });
+
+    describe("toDate method", (): void => {
+        it("should return accept baseDate as a number or a date", (): void => {
+            const baseDate = new Date(2018, 1, 1, 14, 0);
+            const futureDate = new Date(baseDate.getTime() + 300 * 1000);
+            const flex = new FlexTime("", futureDate);
+
+            let dateFromFlex = flex.toDate(undefined, baseDate);
+            expect(dateFromFlex.getUTCDate()).toEqual(baseDate.getUTCDate());
+
+            dateFromFlex = flex.toDate(undefined, baseDate.getTime());
+            expect(dateFromFlex.getUTCDate()).toEqual(baseDate.getUTCDate());
+        });
+
+        it("should return today's date for a time later than now", (): void => {
+            const baseDate = new Date(2018, 1, 1, 14, 0);
+            const futureDate = new Date(baseDate.getTime() + 300 * 1000);
+            const flex = new FlexTime("", futureDate);
+            const dateFromFlex = flex.toDate(undefined, baseDate);
+            expect(dateFromFlex.getUTCDate()).toEqual(baseDate.getUTCDate());
+        });
+
+        it("should return tomorrow's date for a time more than 12 hours later than now", (): void => {
+            const baseDate = new Date(2018, 1, 1, 14, 0);
+            const futureDate = new Date(baseDate.getTime() + (13 * 60 * 60 * 1000));
+            const flex = new FlexTime("", futureDate);
+            const dateFromFlex = flex.toDate(undefined, baseDate);
+            expect(dateFromFlex.getUTCDate()).not.toEqual(baseDate.getUTCDate());
+        });
+
+        it("should return tomorrow's date for a time earlier than now", (): void => {
+            const baseDate = new Date(2018, 1, 1, 14, 0);
+            const pastDate = new Date(baseDate.getTime() - 300 * 1000);
+            const flex = new FlexTime("", pastDate);
+            const dateFromFlex = flex.toDate(undefined, baseDate);
+            expect(dateFromFlex.getUTCDate()).not.toEqual(baseDate.getUTCDate());
+        });
+
+        it("should apply a fudge factor if supplied", (): void => {
+            const baseDate = new Date(2018, 1, 1, 14, 0);
+            const pastDate = new Date(baseDate.getTime() - 300 * 1000);
+            const flex = new FlexTime("", pastDate);
+            const dateFromFlex = flex.toDate(10, baseDate);
+            expect(dateFromFlex.getUTCDate()).toEqual(baseDate.getUTCDate());
+        });
+
+        it("should use now if no base date is supplied", (): void => {
+            const futureDate = new Date(Date.now() + 300 * 1000);
+            const pastDate = new Date(Date.now() - 300 * 1000);
+            const futureFlex = new FlexTime("", futureDate);
+            const pastFlex = new FlexTime("", pastDate);
+            const futureDateFromFlex = futureFlex.toDate();
+            const pastDateFromFlex = pastFlex.toDate();
+            const nowUTCDate = new Date().getUTCDate();
+            expect(futureDateFromFlex.getUTCDate()).toEqual(nowUTCDate);
+            expect(pastDateFromFlex.getUTCDate()).not.toEqual(nowUTCDate);
+        });
+    });
+
+    describe("toString method", (): void => {
+        it("should return properly formatted strings", (): void => {
+            [
+                ["0015", "00", "12", "15", "am"],
+                ["0620", "06", "06", "20", "am"],
+                ["0903", "09", "09", "03", "am"],
+                ["1230 pm", "12", "12", "30", "pm"],
+                ["2359", "23", "11", "59", "pm"],
+            ].forEach((test): void => {
+                let time = new FlexTime(test[0]);
+                let expected = `${test[1]}${test[3]}`;
+                expect(time.toString("HHMM")).toBe(expected);
+                expect(time.toString("HHmm")).toBe(expected);
+
+                expected = `${test[1]}:${test[3]}`;
+                expect(time.toString("HH:MM")).toBe(expected);
+                expect(time.toString("HH:mm")).toBe(expected);
+
+                expected = `${Number(test[2])}${test[3]} ${test[4].toLowerCase()}`;
+                expect(time.toString("hhmm tt")).toBe(expected);
+                expect(time.toString("hhMM tt")).toBe(expected);
+
+                expected = `${Number(test[2])}${test[3]} ${test[4].toUpperCase()}`;
+                expect(time.toString("hhmm TT")).toBe(expected);
+                expect(time.toString("hhMM TT")).toBe(expected);
+
+                expected = `${Number(test[2])}${test[3]}${test[4][0].toLowerCase()}`;
+                expect(time.toString("hhmmt")).toBe(expected);
+                expect(time.toString("hhMMt")).toBe(expected);
+
+                expected = `${Number(test[2])}${test[3]}${test[4][0].toUpperCase()}`;
+                expect(time.toString("hhmmT")).toBe(expected);
+                expect(time.toString("hhMMT")).toBe(expected);
+            });
+        });
+
+        it("should default to 12-hour lower case format", (): void => {
+            [
+                ["0015", "12:15 am"],
+                ["0620", "6:20 am"],
+                ["1230 pm", "12:30 pm"],
+                ["2359", "11:59 pm"],
+            ].forEach((test): void => {
+                let time = new FlexTime(test[0]);
+                expect(time.toString()).toBe(test[1]);
+            });
+        });
+    });
+
+    describe("getDeltaInMinutes", (): void => {
+        it("should choose the nearest possible time", (): void => {
+            [
+                ["1130am", "1230pm", 60],
+                ["2345", "0015", 30],
+                ["1215pm", "1145am", -30],
+                ["0030", "2330", -60],
+                ["0601", "1759", 12 * 60 - 2],
+            ].forEach((test): void => {
+                let t1 = new FlexTime(test[0]);
+                let t2 = new FlexTime(test[1]);
+                expect(t1.getDeltaInMinutes(t2)).toBe(test[2]);
+            });
+        });
+    });
+
+    describe("getAbsoluteDeltaInMinutes", (): void => {
+        it("should not choose the nearest possible time", (): void => {
+            [
+                ["1130am", "1230pm", 60],
+                ["2345", "0015", -(23.5 * 60)],
+                ["1215pm", "1145am", -30],
+                ["0030", "2330", (23 * 60)],
+                ["0601", "1759", 12 * 60 - 2],
+            ].forEach((test): void => {
+                let t1 = new FlexTime(test[0]);
+                let t2 = new FlexTime(test[1]);
+                expect(t1.getAbsoluteDeltaInMinutes(t2)).toBe(test[2]);
+            });
+        });
+    });
+});
